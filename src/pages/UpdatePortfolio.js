89c1c7f0ea7bd5f7d17e10175bdc0c915661762f
DON <donnymrshd.94@gmail..com>
2021-10-31 12:15:24 +0700

    updates

--- src/pages/UpdatePortfolio.js
+++ src/pages/UpdatePortfolio.js
@@ -9,13 +9,13 @@ const UpdatePortfolio = () => {
       <Navbar />
       <div className="custom-page add-portfolio d-flex justify-content-center align-items-center">
         <form className="box p-4">
-          <h3 className="text-center">Add Portfolio</h3>
+          <h3 className="text-center">Update Portfolio</h3>
           <label htmlFor="title">Title</label>
           <br />
           <input type="text" name="title" id="title" />
           <br />
           <button type="submit" className="btn-custom">
-            Add Portfolio
+            Update Portfolio
           </button>
         </form>
       </div>
